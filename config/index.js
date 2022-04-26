502b8fc3a841d08da9d71caef57f7e27cc718470
Tabu Hana <taboodog@hotmail.com>
2022-04-26 06:07:37 -0700

    think done

--- config/index.js
+++ config/index.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const { Sequelize } = require('sequelize')
 
 module.exports = new Sequelize(process.env.JAWSDB_URL ? process.env.JAWSDB_URL : process.env.LOCAL_URL)
--- seeds/postSeeds.js
+++ seeds/postSeeds.js
@@ -6,9 +6,8 @@ const postData = [
     body: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti blanditiis natus at, sunt a officia minima dolores consectetur harum minus est expedita animi eos, tempora eaque? Rerum sint excepturi esse.`
   },
   {
-    title: 'MacBook Pro',
-    body: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, blanditiis ipsa, fugiat voluptates labore quos aut commodi ipsum iure deserunt, voluptatum corrupti sunt harum temporibus quia veniam ullam eius ratione?
-    Doloremque voluptates libero beatae corrupti porro est repellat neque, dolorem, accusantium in minima officiis voluptatibus, magni quam vitae commodi maxime. Quisquam, neque iusto molestias totam obcaecati porro exercitationem sequi excepturi?`
+    title: 'Sample post 2',
+    body: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore reprehenderit magnam doloremque. Libero maiores eveniet eos nesciunt fugit perferendis temporibus culpa! Sint quos vero atque consectetur recusandae quod rerum expedita.`
   }
 ]
 
